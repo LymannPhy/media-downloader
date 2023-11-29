aafbba7b55dde59d97ef9dcbfe77a280db1f9327
Galaxy092 <096polideth@gmail.com>
2023-11-29 15:00:15 +0700

    update service package

--- src/co/cstad/service/DownloadService.java
+++ src/co/cstad/service/DownloadService.java
@@ -1,8 +1,5 @@
 package co.cstad.service;
 
-import java.io.IOException;
-package co.cstad.service;
-
 public interface DownloadService {
     void downloadMedia(String mediaUrl, String format) throws Exception;
 }
